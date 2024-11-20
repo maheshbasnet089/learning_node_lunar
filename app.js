2cea9c574afec93484b7dc59de785c3f16bb0b90
maheshbasnet089 <basnetmanish089@gmail.com>
2024-11-20 10:13:13 +0545

    cors

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var express = require("express") // function
 const DatabaseConnect = require("./database")
 const Blog = require("./model/blogModel")
 var app = express()
+var cors = require('cors')
 
+app.use(cors({
+    origin : "http://localhost:5173"
+}))
 app.use(express.json())
 DatabaseConnect()
 
@@ -23,18 +27,46 @@ app.post("/blog",async function(req,res){
         subtitle : subtitle, 
         description : description
     })
-    res.json({
+    res.status(200).json({
         message : "Blog created successfully"
     })
 })
 
 app.get("/blog",async function(req,res){
         var blogs = await Blog.find() // array 
-        res.json({
+        res.status(200).json({
             data : blogs
         })
 })
 
+app.get("/blog/:id",async function(req,res){
+    var id = req.params.id
+    var blog = await Blog.findById(id) // always returns object 
+    res.status(200).json({
+        data : blog
+    })
+})
+
+app.delete("/blog/:id",async function(req,res){
+    var id = req.params.id
+    await Blog.findByIdAndDelete(id)
+    res.status(200).json({
+        message : "Blog deleted successfully"
+    })
+})
+
+app.patch("/blog/:id",async function(req,res){
+    var id = req.params.id
+    var{title,subtitle,description} = req.body
+    await Blog.findByIdAndUpdate(id,{
+        title , 
+        subtitle , 
+        description
+    })
+    res.status(200).json({
+        message : "Blog updated successfully"
+    })
+})
 
 app.listen(3000,()=>{
     console.log("Nodejs server has started at port 3000")
